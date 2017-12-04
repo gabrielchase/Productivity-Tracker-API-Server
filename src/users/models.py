da94e4c9ab3bbda1db8d880dea5ee4da4fb3edb6
gabrielchase <gchase.patron@gmail.com>
2017-12-04 20:39:26 +0800

    Add `UserManager` and change `User` to inherit from `AbstractUser`

--- src/users/models.py
+++ src/users/models.py
@@ -1,26 +1,55 @@
 from django.db import models
 
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser,)
+    BaseUserManager, AbstractUser,)
 
 
-class User(AbstractBaseUser):
+class UserManager(BaseUserManager):
 
-    first_name = models.CharField(max_length=255, unique=False, null=True)
-    last_name = models.CharField(max_length=255, unique=False, null=True)
+    def create_user(self, first_name, last_name, email, password=None):
+
+        if not email:
+            raise ValueError('Users must register an email address')
+
+        if self.model.objects.filter(email=email).first():
+            raise ValueError('Email address is already taken')
+
+        username = '{}{}'.format(first_name, last_name)
+
+        if self.model.objects.filter(username=username).first():
+            instances = self.model.objects.filter(first_name=first_name, last_name=last_name).count()
+            print("There are {} instances of '{} {}'. Appending {} to new_user's username"
+                .format(instances, first_name, last_name, instances+1))
+            username += '-{}'.format(instances + 1)
+            print('Username created for new_user: {}'.format(username))
+
+        new_user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            username=username
+        )
+
+        new_user.set_password(password)
+        new_user.save(using=self._db)
+
+        return new_user
+
+
+class User(AbstractUser):
+
     email = models.EmailField(max_length=255, unique=True, null=False)
-    slug = models.SlugField(unique=True)
 
-    # objects = BaseUserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # removes email from REQUIRED_FIELDS
 
     def get_full_name(self):
-        return '{}, {}'.format(self.last_name, self.first_naem)
+        return '{}, {}'.format(self.last_name, self.first_name)
 
     def get_short_name(self):
-        return self.email
+        return self.username
 
     def __str__(self):
-        return '{}: {} | {}'.format(self.id, self.get_full_name(), self.email)
-
+        return '({}, {}, {}, {}, {})'.format(self.id, self.first_name, self.last_name, self.email, self.username)
