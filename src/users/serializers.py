46795d1a660e178a2d3beb6536484ebe8b3f0c4f
gabrielchase <gchase.patron@gmail.com>
2017-12-06 20:00:43 +0800

    Rename argument

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -29,27 +29,27 @@ class UserSerializer(serializers.ModelSerializer):
             'password': { 'write_only': True }
         }
 
-    def create(self, validated_data):
-        details = validated_data.pop('details')
+    def create(self, data):
+        details = data.pop('details')
         new_user = User.objects.create_user(
-            first_name=validated_data.get('first_name'),
-            last_name=validated_data.get('last_name'),
-            email=validated_data.get('email'),
+            first_name=data.get('first_name'),
+            last_name=data.get('last_name'),
+            email=data.get('email'),
             country=details.get('country'),
             goal=details.get('goal'),
             mobile_number=details.get('mobile_number'),
-            password=validated_data.get('password')
+            password=data.get('password')
         )
 
         return new_user
 
-    def update(self, user_instance, validated_data):
-        user_instance.first_name = validated_data.get('first_name')
-        user_instance.last_name = validated_data.get('last_name')
-        user_instance.email = validated_data.get('email')
+    def update(self, user_instance, data):
+        user_instance.first_name = data.get('first_name')
+        user_instance.last_name = data.get('last_name')
+        user_instance.email = data.get('email')
         user_instance.set_username()
         
-        details = validated_data.get('details')
+        details = data.get('details')
 
         if details:
             try:
@@ -70,4 +70,3 @@ class UserSerializer(serializers.ModelSerializer):
         user_instance.save()
 
         return user_instance
-        
