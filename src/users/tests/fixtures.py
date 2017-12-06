f15c046a364b5efe8443ba2a66a4132baf4e79bd
gabrielchase <gchase.patron@gmail.com>
2017-12-06 23:05:57 +0800

    Create user fixtures to mock json

--- src/users/tests/fixtures.py
+++ src/users/tests/fixtures.py
@@ -7,7 +7,6 @@ from users.tests.utils import generate_random_password
 
 @pytest.fixture
 def new_user_info():
-    
     return {
         'first_name': mixer.faker.first_name(),
         'last_name': mixer.faker.last_name(),
@@ -16,6 +15,49 @@ def new_user_info():
         'mobile_number': mixer.faker.phone_number(),
         'goal': mixer.faker.text(),
         'password': generate_random_password()
+    }
+    
+@pytest.fixture
+def json_user_with_details():
+    return {
+        'first_name': mixer.faker.first_name(),
+        'last_name': mixer.faker.last_name(),
+        'email': mixer.faker.email(),
+        'password': generate_random_password(),
+        'details': {
+            'country': mixer.faker.country(),
+            'mobile_number': mixer.faker.phone_number(),
+            'goal': mixer.faker.text()
+        }
+    }
+
+@pytest.fixture
+def json_user_with_details_with_one_null_detail():
+    return {
+        'first_name': mixer.faker.first_name(),
+        'last_name': mixer.faker.last_name(),
+        'email': mixer.faker.email(),
+        'password': generate_random_password(),
+        'details': {
+            'country': mixer.faker.country(),
+            'mobile_number': mixer.faker.phone_number(),
+            'goal': None
+        }
+    }
+
+
+@pytest.fixture
+def json_user_no_details():
+    return {
+        'first_name': mixer.faker.first_name(),
+        'last_name': mixer.faker.last_name(),
+        'email': mixer.faker.email(),
+        'password': generate_random_password(),
+        'details': {
+            'country': None,
+            'mobile_number': None,
+            'goal': None
+        }
     }
     
 
