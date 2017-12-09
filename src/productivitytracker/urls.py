85ae250a224d489b96e439bc23d930279879e340
gabrielchase <gchase.patron@gmail.com>
2017-12-09 23:14:28 +0800

    Add JWT authentication, refresh, and verification

--- src/productivitytracker/urls.py
+++ src/productivitytracker/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework_jwt.views import (obtain_jwt_token, refresh_jwt_token, verify_jwt_token)
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
 
@@ -10,5 +11,8 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/token-auth/', obtain_jwt_token),
+    path('api/token-refresh/', refresh_jwt_token),
+    path('api/token-verify/', verify_jwt_token),
 ]
