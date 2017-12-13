8d1f1759c35f2348c072128cf7c1e6544f62b6bd
gabrielchase <gchase.patron@gmail.com>
2017-12-13 22:29:35 +0800

    Create a function to return user's jwt token

--- src/users/tests/utils.py
+++ src/users/tests/utils.py
@@ -1,3 +1,7 @@
+from rest_framework_jwt.settings import api_settings
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 import random
 import string
 
@@ -5,3 +9,15 @@ import string
 def generate_random_password():
     N = random.randint(7, 30)
     return ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=N))
+
+def get_jwt_token(email, password):
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+    user = User.objects.get(email=email)
+    
+    if user.check_password(password):
+        payload = jwt_payload_handler(user)
+        return jwt_encode_handler(payload)
+    else:
+        return False
