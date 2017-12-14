4117b66374dae769476dd3e859f1fa224633a32d
gabrielchase <gchase.patron@gmail.com>
2017-12-14 17:09:32 +0800

    Make fields strings

--- src/activities/serializers.py
+++ src/activities/serializers.py
@@ -4,14 +4,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 from activities.models import (Activity, Category)
-from activities.serializers import UserSerializer
+from users.serializers import UserSerializer
 
 
 class CategorySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Category
-        fields = ('name')
+        fields = ('name',)
 
 
 class ActivitySerializer(serializers.ModelSerializer):
@@ -21,4 +21,4 @@ class ActivitySerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = Activity
-        fields = (id, name, description, start_time, end_time, productive, user, category)
+        fields = ('id', 'name', 'description', 'start_time', 'end_time', 'productive', 'user', 'category')
