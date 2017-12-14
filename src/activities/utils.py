b02caedb2d04c60c58b3200173efdd7a7cad0db6
gabrielchase <gchase.patron@gmail.com>
2017-12-15 01:01:40 +0800

    Make `handle_activity_category` query for Category and return the instance

--- src/activities/utils.py
+++ src/activities/utils.py
@@ -1,2 +1,10 @@
-def handle_category_name(name):
-    return name.lower().title()
+from activities.models import Category
+
+def handle_activity_category(name):
+    category_name = name.lower().title()
+    category = Category.objects.filter(name=category_name).first()
+
+    if not category:
+        category = Category.objects.create(name=category_name)
+
+    return category
