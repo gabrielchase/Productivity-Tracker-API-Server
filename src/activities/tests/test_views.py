c58ba570c080e5ea09b7202b88b64ea0ff8bdd9a
gabrielchase <gchase.patron@gmail.com>
2017-12-16 23:38:32 +0800

    Test for `Activity` post and retrieve get

--- src/activities/tests/test_views.py
+++ src/activities/tests/test_views.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.core.serializers.json import DjangoJSONEncoder
 from mixer.backend.django import mixer
 from rest_framework.test import APIRequestFactory
 
 from users.tests.utils import get_jwt_header
-from users.tests.fixtures import new_user_info
+from users.tests.fixtures import (new_user_info, other_new_user_info)
 from activities.models import (Category, Activity)
+from activities.views import ActivityViewSet
+from activities.utils import handle_activity_category
 from activities.tests.fixtures import new_activity_info
 
 import pytest
@@ -17,86 +20,63 @@ factory = APIRequestFactory()
 ACTIVITIES_URI = 'api/activities' 
 
 
-class TestActivitiesViews(TestCase):
+class TestActivitiesViews:
 
-    def setUp(self):
-        nui = new_user_info()
-        u1 = User.objects.create_user(
-            first_name=nui['first_name'],
-            last_name=nui['last_name'],
-            email=nui['email'],
-            country=nui['country'],
-            mobile_number=nui['mobile_number'],
-            goal=nui['goal'],
-            password=nui['password']
+    def test_view_create_activity(self, new_user_info, new_activity_info):
+        # Create first user
+        User.objects.create_user(
+            first_name=new_user_info['first_name'],
+            last_name=new_user_info['last_name'],
+            email=new_user_info['email'],
+            country=new_user_info['country'],
+            mobile_number=new_user_info['mobile_number'],
+            goal=new_user_info['goal'],
+            password=new_user_info['password']
         )
 
-        nui = new_user_info()
-        u2 = User.objects.create_user(
-            first_name=nui['first_name'],
-            last_name=nui['last_name'],
-            email=nui['email'],
-            country=nui['country'],
-            mobile_number=nui['mobile_number'],
-            goal=nui['goal'],
-            password=nui['password']
+        # Make datetimes strings and create new category
+        new_activity_info['start_time'] = str(new_activity_info['start_time'])
+        new_activity_info['end_time'] = str(new_activity_info['end_time'])
+        new_activity_info['category']['name'] = handle_activity_category(new_activity_info['category']['name']).name
+
+        view = ActivityViewSet.as_view({'post': 'create'})
+        request = factory.post(
+            ACTIVITIES_URI, 
+            data=json.dumps(new_activity_info), 
+            HTTP_AUTHORIZATION=get_jwt_header(new_user_info['email'], new_user_info['password']),
+            content_type='application/json')
+        response = view(request)
+
+        activity_id = response.data.get('id')
+        
+        assert response.status_code == 201
+        assert activity_id
+        assert response.data.get('name') == new_activity_info['name']
+        assert response.data.get('description') == new_activity_info['description']
+        assert response.data.get('start_time') == str(new_activity_info['start_time'].replace(' ', 'T').split('+')[0]+'Z')
+        assert response.data.get('end_time') == str(new_activity_info['end_time'].replace(' ', 'T').split('+')[0]+'Z')
+        assert response.data.get('user_details', {}).get('id')
+        assert response.data.get('user_details', {}).get('email') == new_user_info['email']
+        assert response.data.get('category', {}).get('name') == new_activity_info['category']['name']
+
+        onui = other_new_user_info()
+        User.objects.create_user(
+            first_name=onui['first_name'],
+            last_name=onui['last_name'],
+            email=onui['email'],
+            country=onui['country'],
+            mobile_number=onui['mobile_number'],
+            goal=onui['goal'],
+            password=onui['password']
         )
 
-        category_work = Category.objects.create(name='Work')
-        category_school = Category.objects.create(name='School')
-
-        nai = new_activity_info()
-
-        Activity.objects.create(
-            name=nai['name'],
-            description=nai['description'],
-            start_time=nai['start_time'],
-            end_time=nai['end_time'],
-            productive=nai['productive'],
-            user=u1,
-            category=category_work
+        # u2 can't GET created activity 
+        bad_retrieve = ActivityViewSet.as_view({'get': 'retrieve'})
+        bad_retrieve_request = factory.get(
+            ACTIVITIES_URI+'/{}'.format(activity_id), 
+            HTTP_AUTHORIZATION=get_jwt_header(onui['email'], onui['password'])
         )
+        bad_retrieve_response = view(bad_retrieve_request, pk=activity_id)
 
-        nai = new_activity_info()
-
-        Activity.objects.create(
-            name=nai['name'],
-            description=nai['description'],
-            start_time=nai['start_time'],
-            end_time=nai['end_time'],
-            productive=nai['productive'],
-            user=u1,
-            category=category_school
-        )
-
-        nai = new_activity_info()
-
-        Activity.objects.create(
-            name=nai['name'],
-            description=nai['description'],
-            start_time=nai['start_time'],
-            end_time=nai['end_time'],
-            productive=nai['productive'],
-            user=u2,
-            category=category_work
-        )
-
-        nai = new_activity_info()
-
-        Activity.objects.create(
-            name=nai['name'],
-            description=nai['description'],
-            start_time=nai['start_time'],
-            end_time=nai['end_time'],
-            productive=nai['productive'],
-            user=u2,
-            category=category_school
-        )
-
-    def test_activity_permissions(self):
-
-        print(User.objects.all())
-        print(Category.objects.all())
-        print(Activity.objects.all())
-
-        assert 1 == 2
+        assert bad_retrieve_response.status_code == 405
+        
